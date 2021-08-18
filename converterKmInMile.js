ab523c1abc1a768bc92a623663301780f3a8a2db
Tony <un_razum@mail.ru>
2021-08-18 23:09:42 +0300

    update
    update

--- converterKmInMile.js
+++ converterKmInMile.js
@@ -1,3 +1,10 @@
-const kmInput = document.querySelector('#km');
-const mileInput = document.querySelector('#miles');
-const allInput = document.querySelector('input');
+const kmInput = document.querySelector("#km");
+const mileInput = document.querySelector("#miles");
+const allInput = document.querySelectorAll("input");
+
+allInput.forEach(function (input) {
+  input.addEventListener("input", function (e) {
+    let number = parseInt(e.target.value);
+    let inputName = e.target.name;
+  });
+});

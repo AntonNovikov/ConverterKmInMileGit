afe8b60fa3a627e3a99ef5bd1cb46d1b0ed70617
Tony <un_razum@mail.ru>
2021-08-18 23:37:25 +0300

    Int Float

--- converterKmInMile.js
+++ converterKmInMile.js
@@ -4,7 +4,8 @@ const allInput = document.querySelectorAll("input");
 
 allInput.forEach(function (input) {
   input.addEventListener("input", function (e) {
-    let number = parseInt(e.target.value);
+    // let number = parseInt(e.target.value);
+    let floatNumber = parseFloat(e.target.value);
     let inputName = e.target.name;
 
     if (e.target.value == "") {
@@ -14,10 +15,12 @@ allInput.forEach(function (input) {
     }
 
     if (inputName === "km") {
-      let milesValue = number / 1.609344;
+    //   let milesValue = number / 1.609344;
+      let milesValue = floatNumber/ 1.609344;
       mileInput.value = milesValue.toFixed(5);
     } else {
-      let kmValue = number * 1.609344;
+    //   let kmValue = number * 1.609344;
+      let kmValue = floatNumber* 1.609344;
       kmInput.value = kmValue.toFixed(5);
     }
   });
